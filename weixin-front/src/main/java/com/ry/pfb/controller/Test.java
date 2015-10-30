0a3ed06abed04063fab31b38a64ac5ca939b74b6
workInJava <wanghuaminhappy@163.com>
2015-10-30 15:24:00 +0800

    weixin

--- weixin-front/src/main/java/com/ry/pfb/controller/Test.java
+++ weixin-front/src/main/java/com/ry/pfb/controller/Test.java
@@ -1,5 +1,6 @@
 package com.ry.pfb.controller;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,14 +11,76 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.ry.pfb.core.WxMpServerUtil;
+
+import me.chanjar.weixin.common.util.StringUtils;
+import me.chanjar.weixin.mp.api.WxMpConfigStorage;
+import me.chanjar.weixin.mp.api.WxMpMessageRouter;
+import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.bean.WxMpXmlMessage;
+import me.chanjar.weixin.mp.bean.WxMpXmlOutMessage;
+
 @Controller
 public class Test {
 
+	private final WxMpConfigStorage wxMpConfigStorage = WxMpServerUtil.wxMpConfigStorage;
+	private final WxMpService wxMpService = WxMpServerUtil.wxMpService;
+	private final WxMpMessageRouter wxMpMessageRouter = WxMpServerUtil.wxMpMessageRouter;
+
 	@RequestMapping("/test")
 	@ResponseBody
-	public ModelAndView method(HttpServletRequest re,HttpServletResponse res) throws UnsupportedEncodingException{
+	public ModelAndView method(HttpServletRequest re, HttpServletResponse res) throws UnsupportedEncodingException {
 		ModelAndView mv = new ModelAndView("test");
 		mv.addObject("name", "测试");
-		return mv ;
+		return mv;
+	}
+
+	@RequestMapping("/testweixin")
+	public void test(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.setContentType("text/html;charset=utf-8");
+		response.setStatus(HttpServletResponse.SC_OK);
+
+		String signature = request.getParameter("signature");
+		String nonce = request.getParameter("nonce");
+		String timestamp = request.getParameter("timestamp");
+
+		if (!wxMpService.checkSignature(timestamp, nonce, signature)) {
+			// 消息签名不正确，说明不是公众平台发过来的消息
+			response.getWriter().println("非法请求");
+			return;
+		}
+
+		String echostr = request.getParameter("echostr");
+		if (StringUtils.isNotBlank(echostr)) {
+			// 说明是一个仅仅用来验证的请求，回显echostr
+			response.getWriter().println(echostr);
+			return;
+		}
+
+		String encryptType = StringUtils.isBlank(request.getParameter("encrypt_type")) ? "raw"
+				: request.getParameter("encrypt_type");
+
+		if ("raw".equals(encryptType)) {
+			// 明文传输的消息
+			WxMpXmlMessage inMessage = WxMpXmlMessage.fromXml(request.getInputStream());
+			WxMpXmlOutMessage outMessage = wxMpMessageRouter.route(inMessage);
+			if (outMessage != null) {
+				response.getWriter().write(outMessage.toXml());
+			}
+			return;
+		}
+
+		if ("aes".equals(encryptType)) {
+			// 是aes加密的消息
+			String msgSignature = request.getParameter("msg_signature");
+			WxMpXmlMessage inMessage = WxMpXmlMessage.fromEncryptedXml(request.getInputStream(), wxMpConfigStorage,
+					timestamp, nonce, msgSignature);
+			WxMpXmlOutMessage outMessage = wxMpMessageRouter.route(inMessage);
+			response.getWriter().write(outMessage.toEncryptedXml(wxMpConfigStorage));
+			return;
+		}
+
+		response.getWriter().println("不可识别的加密类型");
+		return;
 	}
 }
