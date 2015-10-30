644c6c0ff09d83a714094c5c9f27b25b93d51d34
workInJava <wanghuaminhappy@163.com>
2015-10-30 13:36:26 +0800

    view测试

--- weixin-front/src/main/java/com/ry/pfb/controller/Test.java
+++ weixin-front/src/main/java/com/ry/pfb/controller/Test.java
@@ -8,14 +8,16 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class Test {
 
 	@RequestMapping("/test")
 	@ResponseBody
-	public String method(HttpServletRequest re,HttpServletResponse res) throws UnsupportedEncodingException{
-			String name="哈哈哈";
-		return name;
+	public ModelAndView method(HttpServletRequest re,HttpServletResponse res) throws UnsupportedEncodingException{
+		ModelAndView mv = new ModelAndView("test");
+		mv.addObject("name", "测试");
+		return mv ;
 	}
 }
